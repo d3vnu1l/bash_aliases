0bafcd2df457bfde60e2768dd1cfec496b763019
d3vnu1l <rdeushane@pm.me>
2025-02-12 10:27:47 -0800

    add free

--- misc_aliases.sh
+++ misc_aliases.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 alias bt="btop"
+alias cpr="cp -r"
 alias dfh="df -h"
 alias fdl="fdisk -l"
+alias fr="free -h"
 alias ll="ls -lah"
 alias ra="ranger"
 alias sudo="sudo " # Support using aliases with sudo
