187e519c1cdbc0dfdab781e883f472e588b4539e
d3vnu1l <rdeushane@pm.me>
2025-02-11 22:47:13 -0800

    more aliases

--- misc_aliases.sh
+++ misc_aliases.sh
@@ -2,6 +2,7 @@
 
 alias bt="btop"
 alias dfh="df -h"
+alias fdl="fdisk -l"
 alias ll="ls -lah"
 alias ra="ranger"
 alias sudo="sudo " # Support using aliases with sudo
--- zfs_aliases.sh
+++ zfs_aliases.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+alias zfg="zfs get"
+alias zfga="zfs get all"
 alias zfl="zfs list"
 alias zfls="zfs list -t snapshot"
 alias zfsr="zfs snapshot -r"
