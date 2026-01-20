866d956daffc98d20876ba39cbd5021606433a1b
d3vnu1l <radeushane@pm.me>
2026-01-19 21:10:19 -0800

    pnused

--- misc_aliases.sh
+++ misc_aliases.sh
@@ -9,6 +9,8 @@ alias ll="ls -lah"
 alias llg="ls -lah | grep"
 alias ra="ranger"
 
+alias punused="pacman -Rns $(pacman -Qdtq)"
+
 # These entries ensure aliases are expanded before using aliases as a subcommand of another command.
 alias sudo="sudo "
 alias watch="watch "
